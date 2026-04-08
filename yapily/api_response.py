b15c555b8a7aaa2fc0667af2d5af71b1d5b9c5af
Johnny Deuss <6266815+JohnnyDeuss@users.noreply.github.com>
2026-04-08 15:16:32 +0100

    Migrate to Pydantic 2 and ruff

--- yapily/api_response.py
+++ yapily/api_response.py
@@ -1,7 +1,9 @@
 """API response object."""
 
 from __future__ import annotations
-from typing import Any, Dict, Optional
+
+from typing import Any
+
 from pydantic import Field, StrictInt, StrictStr
 
 
@@ -10,14 +12,14 @@ class ApiResponse:
     API response object
     """
 
-    status_code: Optional[StrictInt] = Field(None, description="HTTP status code")
-    headers: Optional[Dict[StrictStr, StrictStr]] = Field(
+    status_code: StrictInt | None = Field(None, description="HTTP status code")
+    headers: dict[StrictStr, StrictStr] | None = Field(
         None, description="HTTP headers"
     )
-    data: Optional[Any] = Field(
+    data: Any | None = Field(
         None, description="Deserialized data given the data type"
     )
-    raw_data: Optional[Any] = Field(None, description="Raw data (HTTP response body)")
+    raw_data: Any | None = Field(None, description="Raw data (HTTP response body)")
 
     def __init__(
         self, status_code=None, headers=None, data=None, raw_data=None
