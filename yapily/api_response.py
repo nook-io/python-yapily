1702dd83fa41bfb75a5e264aace3c434a0774973
Johnny Deuss <6266815+JohnnyDeuss@users.noreply.github.com>
2026-04-21 13:21:27 +0100

    Remove future imports

--- yapily/api_response.py
+++ yapily/api_response.py
@@ -1,7 +1,5 @@
 """API response object."""
 
-from __future__ import annotations
-
 from typing import Annotated, Any
 
 from pydantic import Field, StrictInt, StrictStr
@@ -12,10 +10,10 @@ class ApiResponse:
     API response object
     """
 
-    status_code: Annotated[StrictInt | None, Field(description='HTTP status code')] = None
-    headers: Annotated[dict[StrictStr, StrictStr] | None, Field(description='HTTP headers')] = None
-    data: Annotated[Any | None, Field(description='Deserialized data given the data type')] = None
-    raw_data: Annotated[Any | None, Field(description='Raw data (HTTP response body)')] = None
+    status_code: Annotated[StrictInt | None, Field(description="HTTP status code")] = None
+    headers: Annotated[dict[StrictStr, StrictStr] | None, Field(description="HTTP headers")] = None
+    data: Annotated[Any | None, Field(description="Deserialized data given the data type")] = None
+    raw_data: Annotated[Any | None, Field(description="Raw data (HTTP response body)")] = None
 
     def __init__(self, status_code=None, headers=None, data=None, raw_data=None) -> None:
         self.status_code = status_code
